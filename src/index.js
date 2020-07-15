17fd887832ca10a38595b35d68027d5f70086692
jamesjlandry <james.j.landry@outlook.com>
2020-07-15 15:04:23 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const newTaskDesc = document.getElementById('new-task-description');
+
+  document.getElementById('create-task-form').addEventListener('submit', event => {
+    event.preventDefault();
+    let text = event.target[0].value;
+    console.log(text);
+    let ul = document.getElementById('tasks');
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML = text
+    
+  })
 });
