cfc6a647016633d56d0de66e101324f7e98dd212
mcgillshakore <mcgills2093@gmail.com>
2020-07-18 19:47:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const taskForm = document.querySelector("#create-task-form");
+
+    const taskList = document.querySelector("#tasks");
+    
+    taskForm.addEventListener('submit', function(event){
+              event.preventDefault()
+            // console.log("I was clicked")
+        const newTask = document.querySelector("#new-task-description").value
+
+        taskList.innerHTML = `<li>${newTask}</li> <button id="delete_task">x</button`;
+        const deleteBtn = document.querySelector("#delete_task");
+
+        deleteBtn.addEventListener('click', function(){
+            deleteBtn.parentNode.remove();
+        });
+    });
 });
